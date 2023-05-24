b179f883c46ebb7ff797f9c05eb20f606d81ccaa
Francesco <iudica.francesco@gmail.com>
2023-05-24 16:27:07 +0200

    added customer list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomerListComponent} from "./components/customer-list/customer-list.component";
+
 
 const routes: Routes = [
-  {path:"/alan"},
-  {path:"/robert"},
-  {path:"/francesco"},
+ /* {path:"/alan"},
+  {path:"/robert"},*/
+  {path:"francesco", component : CustomerListComponent},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './module/material.module';
 
+import { HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
