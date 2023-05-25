450d016c5ae25d3f68369319bdfc5b75cdb667bb
robert-bugean <robert.bugean01@gmail.com>
2023-05-25 16:28:11 +0200

    clean app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './module/material.module';
 
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CompanyHomeComponent } from './components/company-home/company-home.component';
-
+import { CompanyDetailsComponent } from './components/company-details/company-details.component';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
-import { CustomerListComponent } from './components/customer-list/customer-list.component';
-
-
-import { FlexLayoutModule } from "@angular/flex-layout";
 import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CompanyHomeComponent,
+    CompanyDetailsComponent,
     EmployeeListComponent,
-    CustomerListComponent,
-    EmployeeDetailComponent
+    EmployeeDetailComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    HttpClientModule,
-    FlexLayoutModule,
+    AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
