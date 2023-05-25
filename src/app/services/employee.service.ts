c9cf7d03ca54f0acc6eaba05c5bfd31f568a98da
Alan Rinaldi <rinaldialan2@gmail.com>
2023-05-25 11:15:06 +0200

    add getEmployeeById in employee service

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -3,7 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { EmployeeDTO } from '../dto/EmployeeDTO';
 import { switchMap, toArray ,from} from 'rxjs';
 
-const urlEmployee="http://localhost:8080/api/employee/"
+const urlAllEmployee="http://localhost:8080/api/employee/"
+const urlEmployeeById="http://localhost:8080/api/employee/id/"
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +15,13 @@ export class EmployeeService {
 
 
   getAllEmployee(){
-    return this.http.get<EmployeeDTO[]>(urlEmployee)
+    return this.http.get<EmployeeDTO[]>(urlAllEmployee)
       .pipe(
         switchMap((array:EmployeeDTO[])=>from(array)),
         toArray()
       )
+  }
+  getEmployeeById(id:number){
+    return this.http.get<EmployeeDTO>(urlAllEmployee+id)
   }
 }
